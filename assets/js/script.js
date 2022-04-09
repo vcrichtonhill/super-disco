2ed6c551b7b7ed4c6488bc9600fe31b3c94f9c06
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-04-09 16:39:35 -0500

    color blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,46 @@
-// display current day
-    // append in header
-
-// time blocks 9am - 5pm
-    // create li to append to container
-    // time-block, row, hour classes
 
 // greyed out for past, red present, green future (use classes)
     // click on block --> enter an event
     // save in local storage
 
-var saveBtn = document.getElementById("save")
+var saveBtn = $(".saveBtn")
 var taskInput = document.getElementById("task-input")
+var currentDay = document.getElementById("currentDay")
+currentDay.innerHTML = moment().format('LL')
+var currentTime = moment().format('LT');
+
 
 let taskStorage = localStorage.getItem("taskInput")
     JSON.stringify(localStorage.getItem("taskInput"))
 
-saveBtn.addEventListener("click", function(){
-    taskStorage.push(taskInput.value.trim());
-    localStorage.setItem("taskInput", JSON.stringify(taskStorage));
-    listBuilder(taskInput.value);
-    taskInput.value = "";
+saveBtn.on("click", function(){
+    // taskStorage.push(taskInput.value.trim());
+    // localStorage.setItem("taskInput", JSON.stringify(taskStorage));
+    // listBuilder(taskInput.value);
+    // taskInput.value = "";
+    var activity = $(this).siblings(".description").val()
+    var hourKey = $(this).parent().attr("id")
+    localStorage.setItem(hourKey, activity)
 });
+for (var i = 9; i < 18; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i))
+
+}
+$(".time-block").each(function(row){
+    var hour = $(this).attr("id").split("-")[1]
+    var momentHour = moment().hour()
+    if (hour < momentHour) {
+        $(this).addClass("past")
+    } else if (hour == momentHour) {
+        $(this).addClass("present")
+    } else {
+        $(this).addClass("future")
+    }
+})
+// if (hour is after moment().format('LT');) {
+//     assign class past
+// } else if {
+//     assign class present
+// } else {
+//     assign class future
+// }
